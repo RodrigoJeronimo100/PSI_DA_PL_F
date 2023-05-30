53beb3271d317b543b26b58e35ec7a83b328997e
lourencoindio <2220908@my.ipleiria.pt>
2023-05-30 23:26:42 +0100

    cinema

--- ProjetoDA/ProjetoDA/FormCinema.cs
+++ ProjetoDA/ProjetoDA/FormCinema.cs
@@ -6,27 +6,18 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.Design.WebControls;
 using System.Windows.Forms;
 
 namespace ProjetoDA
 {
     public partial class FormCinema : Form
     {
-        private FormSala1 formSala1;
+ 
         public FormCinema()
         {
             InitializeComponent();
-        }
 
-        private void sala1ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-            if (formSala1 == null)
-            {
-                formSala1 = new FormSala1();
-                formSala1.FormClosed += (s, args) => formSala1 = null;
-                formSala1.Show();
-            }
         }
     }
 }
