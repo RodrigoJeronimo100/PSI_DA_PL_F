23427deaf9c78d8a39d6647262fed2ccda2fccf0
lourencoindio <2220908@my.ipleiria.pt>
2023-06-01 22:00:29 +0100

    PROBLEMA

--- ProjetoDA/ProjetoDA/FormCinema.Designer.cs
+++ ProjetoDA/ProjetoDA/FormCinema.Designer.cs
@@ -29,21 +29,68 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCinema));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sala1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sala2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sala1ToolStripMenuItem,
+            this.sala2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sala1ToolStripMenuItem
+            // 
+            this.sala1ToolStripMenuItem.Name = "sala1ToolStripMenuItem";
+            this.sala1ToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.sala1ToolStripMenuItem.Text = "Sala 1";
+            this.sala1ToolStripMenuItem.Click += new System.EventHandler(this.sala1ToolStripMenuItem_Click);
+            // 
+            // sala2ToolStripMenuItem
+            // 
+            this.sala2ToolStripMenuItem.Name = "sala2ToolStripMenuItem";
+            this.sala2ToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.sala2ToolStripMenuItem.Text = "Sala 2";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(12, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 411);
+            this.tabControl1.TabIndex = 1;
             // 
             // FormCinema
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormCinema";
             this.Text = "Cinema";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sala1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sala2ToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
--- ProjetoDA/ProjetoDA/FormCinema.cs
+++ ProjetoDA/ProjetoDA/FormCinema.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,24 +6,18 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.UI.Design.WebControls;
 using System.Windows.Forms;
 
 namespace ProjetoDA
 {
     public partial class FormCinema : Form
     {
- 
+        private FormSala1 formSala1;
         public FormCinema()
         {
             InitializeComponent();
-<<<<<<< HEAD
-<<<<<<< HEAD
         }
-        private void Cinema_Load(object sender, EventArgs e)
-        {
-<<<<<<< HEAD
-        }
+
         private void textBoxNome_TextChanged(object sender, EventArgs e)
         {
 
@@ -46,26 +40,8 @@ namespace ProjetoDA
             }
 =======
 
->>>>>>> parent of bf77574 (Merge branch 'main' of https://github.com/RodrigoJeronimo100/PSI_DA_PL_F)
-        }
-
-        private int contadorSalas = 1;
-        private void button_criarsala_Click(object sender, EventArgs e)
-        {
-            if (textBox_novasala.Text != "")
-            {
-                TabPage sala = new TabPage();
-                sala.Text = "Sala" + contadorSalas;
-                sala.Name = "Sala" + contadorSalas;
-                sala.TabIndex = tabControl1.TabPages.Count;
-                tabControl1.TabPages.Add(sala);
-                textBox_novasala.Clear();
-
-                contadorSalas++;
-            }
-        }
 =======
-
+>>>>>>> 53beb3271d317b543b26b58e35ec7a83b328997e
         }
 >>>>>>> parent of 2951ab5 (1)
 =======
